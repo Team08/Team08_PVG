afeab6e8b782b5eee3f1546b0839eada1667d303
Kristofer Jacobson <dt08kj7@student.lth.se>
2012-01-17 13:20:24 +0100

    Added Sorter.java

---
+++ src/main/Sorter.java
@@ -0,0 +1,40 @@
+package main;
+
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+
+public class Sorter {
+
+	private String stopFile;
+	private String startFile;
+
+	public Sorter(String startFileName, String stopFileName) {
+		this.startFile = startFileName;
+		this.stopFile = startFileName;
+	}
+	
+	public static void main(String[] args) {
+		Sorter sorter = new Sorter("Start.txt","Stop.txt");
+		sorter.writeResultFile();
+	}
+
+	private boolean writeResultFile() {
+		try {
+			// Create file
+			FileWriter fstream = new FileWriter("Result");
+			BufferedWriter out = new BufferedWriter(fstream);
+			
+
+			out.write("This is the results");
+			// Close the output stream
+			out.close();
+			
+		} catch (Exception e) {// Catch exception if any
+			System.err.println("Error: " + e.getMessage());
+			return false;
+		}
+		return true;
+	}
+}
