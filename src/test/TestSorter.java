7b998e7decbac21ee418ce4c9f955ad3141389fc
Christoffer <ada10cla@student.lu.se>
2012-01-23 10:19:47 +0100

    Added test case
    TestSorter.class
    
    testAddStartTime()

--- src/test/TestSorter.java
+++ src/test/TestSorter.java
@@ -2,6 +2,7 @@ package test;
 
 
 import static org.junit.Assert.*;
+import main.Driver;
 import main.Sorter;
 
 import org.junit.After;
@@ -28,6 +29,15 @@ public class TestSorter extends Sorter {
 		String time = "01.01.01";
 		super.addStartTime(i, time);
 		assertEquals(register.size(), 1);
+	}
+	
+	@Test
+	public void testAddStartTime() {
+		Integer i = new Integer(1);
+		String time = "01.01.01";
+		super.addStartTime(i, time);
+		Driver driver = register.get(i);
+		assertEquals(driver.startTime(), "01.01.01");
 	}
 
 }
