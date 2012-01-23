dd06eec8efd39d0c9672b4562c9598cf6e0ae12e
Christoffer <ada10cla@student.lu.se>
2012-01-23 09:50:47 +0100

    Added class TestSorter.java

---
+++ src/test/TestSorter.java
@@ -0,0 +1,17 @@
+package test;
+
+
+import org.junit.After;
+import org.junit.Before;
+
+public class TestSorter {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+}
