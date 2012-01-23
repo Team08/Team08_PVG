380c77ccd99aab7ad0721e20debaa35bd373a4a9
Emma Holmberg Ohlsson <ada10eho@student.lu.se>
2012-01-23 15:41:52 +0100

    6.5 klar

--- src/test/TestSorter.java
+++ src/test/TestSorter.java
@@ -230,5 +230,23 @@ public class TestSorter extends Sorter {
 			e.printStackTrace();
 		}
 	}
-
+	@Test
+	public void testImpossibleFinishTime() throws Exception {
+		try {
+			Integer i = new Integer(1);
+			String start = "12.00.00";
+			super.addStartTime(i, start);
+			String finish = "12.14.00";
+			super.addFinishTime(i, finish);
+			writeResultFile("Result.txt");
+			Scanner sc = new Scanner(new File("Result.txt"));
+			sc.nextLine();
+			assertEquals(
+					"1; --.--.--; 0.14.00; 12.00.00; 12.14.00; Omöjlig Totaltid?",
+					sc.nextLine());
+			sc.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
 }
