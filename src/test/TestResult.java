3c02b618ef7687c81be60b218756a53c512bd5c3
Hawoay <ada09hta@student.lth.se>
2012-01-23 17:07:01 +0100

    Added TestFinish.test

---
+++ src/test/TestResult.java
@@ -0,0 +1,30 @@
+package test;
+
+import static org.junit.Assert.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Set;
+
+import main.ReadNameFile;
+import main.Sorter;
+import org.junit.Test;
+
+public class TestResult extends Sorter {
+
+	public TestResult() {
+		super("TestStart.test", "TestFinish.test", "src/namnfil.test");
+	}
+	
+	//incomplete
+	@Test
+	public void TestwriteResultFile() throws FileNotFoundException{
+		readStartFile();
+		readFinishFile();
+		writeResultFile("Result.txt");
+	}
+	
+	
+
+}
