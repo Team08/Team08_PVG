eb478e442586f758ba020c34bf13e39fec93658b
Hawoay <ada09hta@student.lth.se>
2012-01-23 10:30:13 +0100

    Added new temporary class ReadFile.java added new testclass TestReadFile.java added new metod in sorter readFile.java

--- src/main/Sorter.java
+++ src/main/Sorter.java
@@ -1,12 +1,15 @@
 package main;
 
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
 import java.io.FileWriter;
+import java.io.InputStreamReader;
 
 
 public class Sorter {
-
 	private String stopFile;
 	private String startFile;
 
@@ -36,5 +39,26 @@ public class Sorter {
 			System.exit(1);
 		}
 		return true;
+	}
+	
+	public void readFile(String filename){
+		try{
+			  FileInputStream fstream = new FileInputStream(filename);
+			  // Get the object of DataInputStream
+			  DataInputStream in = new DataInputStream(fstream);
+			  BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			  String strLine;
+			  
+			  //Read File Line By Line
+			  while ((strLine = br.readLine()) != null)   {
+			  // Print the content on the console
+			  System.out.println (strLine);
+			  }
+			  //Close the input stream
+			  in.close();
+			    }catch (Exception e){//Catch exception if any
+			  System.err.println("Error: " + e.getMessage());
+			  }
+		
 	}
 }
