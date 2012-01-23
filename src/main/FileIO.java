e30d938ed52792a1cbf574d3a086a682b85b93dc
Christoffer <ada10cla@student.lu.se>
2012-01-23 17:30:07 +0100

    Refactored a loooot!!

---
+++ src/main/FileIO.java
@@ -0,0 +1,15 @@
+package main;
+
+import java.util.TreeMap;
+
+public class FileIO {
+	protected Sorter sorter;
+	protected String fileName;
+	
+	public FileIO(Sorter sorter, String fileName) {
+		this.sorter = sorter;
+		this.fileName = fileName;
+	}
+	
+	
+}
