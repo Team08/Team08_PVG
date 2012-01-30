421ffa2344dc5cbf120091fca853732fff5f6c8b
Christoffer <ada10cla@student.lu.se>
2012-01-30 09:41:35 +0100

    Fixed merge

--- src/model/Register.java
+++ src/model/Register.java
@@ -34,24 +34,19 @@ public class Register {
 
 	public String[] registerDriver(String name) {
 		GregorianCalendar calendar = new GregorianCalendar();
-		Time time = new Time();
 
 		int hours = calendar.get(Calendar.HOUR_OF_DAY);
 		int minutes = calendar.get(Calendar.MINUTE);
 		int seconds = calendar.get(Calendar.SECOND);
 		String[] times = new String[3];
 
-		// String stringMinutes = new String(minutes + "");
-		// String stringSeconds = new String(seconds + "");
-		// String stringMinutes = Integer.toString(minutes);
-		String stringMinutes = time.addZero(minutes);
-		String stringSeconds = time.addZero(seconds);
+		String stringMinutes = Time.addZero(minutes);
+		String stringSeconds = Time.addZero(seconds);
 
 		times[0] = Integer.toString(hours);
 		times[1] = stringMinutes;
 		times[2] = stringSeconds;
-		// writeToFile(name, new Integer(hours).toString(), stringMinutes,
-		// stringSeconds);
+
 		writeToFile(name, times[0], times[1], times[2]);
 		return times;
 	}
