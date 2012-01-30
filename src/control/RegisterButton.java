475cb79a808e33ba712459de3913d6d2e6dbfeee
Christoffer <ada10cla@student.lu.se>
2012-01-30 09:17:28 +0100

    Allt fint och mergat efter refaktorisering av gui nummer 2

---
+++ src/control/RegisterButton.java
@@ -0,0 +1,30 @@
+package control;
+
+import gui.BasicGUI;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+
+import model.Register;
+
+public class RegisterButton extends JButton implements ActionListener {
+	private BasicGUI gui;
+	private Register register;
+
+	public RegisterButton(BasicGUI gui, Register register) {
+		super("Registrera förare");
+		this.gui = gui;
+		this.register = register;
+		addActionListener(this);
+	}
+
+	public void actionPerformed(ActionEvent arg0) {
+		String name = gui.getDriverText();
+		if (gui.getDriverText().length() != 0) {
+			String[] times = register.registerDriver(name);
+			gui.writeInScrollPane(times[0], times[1], times[2]);
+
+		}
+	}
+
+}
