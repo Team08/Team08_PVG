05bd5a2a17ad2b79298e393563284c7052b46705
Emma Holmberg Ohlsson <ada10eho@student.lu.se>
2012-02-20 10:53:45 +0100

    WTF? Story 10

--- src/reader/ReadFinishFile.java
+++ src/reader/ReadFinishFile.java
@@ -1,6 +1,11 @@
 package reader;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 import race.Race;
+import util.Time;
 
 /**
  * This class can read a finish file and add the finish time + driverId to the
@@ -37,6 +42,33 @@ public class ReadFinishFile extends FileIO {
 	 */
 	protected void printErrorText() {
 		System.err.println("Hittade inte finishfilen.");
+	}
+	
+	@Override
+	public void readFile() throws FileNotFoundException {
+		String[] finishFiles = fileName.split(", ");
+		for(int i = 0; i < finishFiles.length; i++){
+		if (finishFiles[i] != null) {
+			File file = new File(finishFiles[i].trim());
+			Scanner scanner;
+			try {
+				scanner = new Scanner(file);
+				String line;
+
+				while (scanner.hasNextLine()) {
+					line = scanner.nextLine();
+					String[] str = line.split("; ");
+					time = new Time(str[1]);
+					riderID = Integer.parseInt(str[0]);
+					add();
+				}
+
+			} catch (FileNotFoundException e) {// Catch exception if any
+				printErrorText();
+				throw new FileNotFoundException();
+			}
+		}
+		}
 	}
 
 }
